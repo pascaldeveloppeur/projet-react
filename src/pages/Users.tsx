b3e6dc4feb4f4d51032647d9ccabc43a39c447b4
pascalDev <pascalyakouyami@gmail.com>
2025-03-02 01:12:26 +0100

    u

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -15,11 +15,12 @@ type UsersProps = {
 };
 
 function Users(props: UsersProps) {
-    const listUsers = props.users;
+  //destructure the props
+    const {users} = props;
   return (
     <div className="grid md:grid-cols-3 gap-4 my-2">
-        {listUsers.map((user) => (
-            <User key={user._id} name={user.name} age={user.age} gender={user.gender} email={user.email} phone={user.phone} address={user.address} />
+        {users.map(({_id, name, age, gender,email,  phone, address}: User) => (
+            <User key={_id} name={name} age={age} gender={gender} email={email} phone={phone} address={address} />
         ))}
           
     </div>
