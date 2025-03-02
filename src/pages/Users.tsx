a0b97c3b2701c7b1616dba7e305a694a5554dec7
pascalDev <pascalyakouyami@gmail.com>
2025-03-02 01:51:52 +0100

    u

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -14,17 +14,42 @@ type UsersProps = {
     users: User[];
 };
 
-function Users(props: UsersProps) {
-  //destructure the props
-  // const users = props.users;
-    const {users} = props;
+function Users({users}: UsersProps) {
+  let usersSorted: User[] = users;
+  const sortBy = (field: string) => {
+    console.log(field);
+    usersSorted = usersSorted.sort((firstUser: User, secondUser : User) => {
+      const fieldInFirstUser = firstUser[field as keyof User] || "";
+      const fieldInSecondUser = secondUser[field as keyof User] || "";
+      
+
+      if (fieldInFirstUser < fieldInSecondUser) {
+        return -1;
+      }
+      if (fieldInFirstUser > fieldInSecondUser) {
+        return 1;
+      }
+      return 0;
+    });
+  };
+     
   return (
-    <div className="grid md:grid-cols-3 gap-4 my-2">
+    <section >
+      <div className="flex justify-end items-center my-2 text-sm gap-2">
+        <span>Trier par</span>
+        <button type="button" className="border border-gray-100 bg-gray-50 rounded-md px-2 py-1 text-xs"
+        onClick={() => sortBy("male")}>Hommes</button>
+        <button type="button" className="border border-gray-100 bg-gray-50 rounded-md px-2 py-1 text-xs"
+        onClick={() => sortBy("female")}>Femmes</button>
+      </div>
+      <div className="grid md:grid-cols-3 gap-4 my-2">
         {users.map(({_id, name, age, gender,email,  phone, address}: User) => (
-            <User key={_id} name={name} age={age} gender={gender} email={email} phone={phone} address={address} />
-        ))}
+              <User key={_id} name={name} age={age} gender={gender} email={email} phone={phone} address={address} />
+          ))}
+      </div>
+       
           
-    </div>
+    </section>
   );
 }
 
